67972c0c649ac596aca6cbdea6112bc70b7ae832
Autumn R. Mosley <hatbearingoctopus@gmail.com>
2019-07-25 19:02:04 -0700

    Update notes.app.js

--- notes.app.js
+++ notes.app.js
@@ -33,3 +33,36 @@ window.addEventListener('storage', function (e) {
     renderNotes(notes, filters)
   }
 })
+
+//Unix Epoch - Jan 1st 1970 00:00:00
+
+const now = new Date()
+const timeStamp = now.getTime()
+
+const myDate = new Date(timeStamp)
+console.log(myDate.getFullYear())
+
+// console.log(`Year: ${now.getFullYear()})`)
+// console.log(`Month: ${now.getMonth()})`)
+// console.log(`Day: ${now.getDate()})`)
+// console.log(`Hours: ${now.getHours()})`)
+// console.log(`Minutes: ${now.getMinutes()})`)
+// console.log(`Seconds: ${now.getSeconds()})`)
+
+//CHALLEGE
+//1. Create two dates in the past (use string for date)
+const challenge = new Date('June 06 2016 05:55:00')
+const challenge2 = new Date('November 08 2012 05:25:00')
+console.log(challenge)
+console.log(challenge2)
+//2. Get timestamps for both
+const timeStamp1 = challenge.getTime()
+const timeStamp2 = challenge2.getTime()
+console.log(timeStamp1)
+console.log(timeStamp2)
+//3. Figure out which is first and print its time (use toString)
+if (timeStamp1 > timeStamp2) {
+  console.log(challenge2.toString())
+} else if (timeStamp2 > timeStamp1) {
+  console.log(challenge1.toString())
+}
